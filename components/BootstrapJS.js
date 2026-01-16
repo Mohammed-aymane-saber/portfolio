16855ec9f4ef455730392c9e2c9ee8ef397017d6
Mohammed-aymane-saber <aymanesaber13@gmail.com>
2026-01-16 11:53:22 +0100

    delelte all to rebuild

--- components/BootstrapJS.js
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react';
-
-export default function BootstrapJS() {
-  useEffect(() => {
-    import('bootstrap/dist/js/bootstrap.bundle.min.js');
-  }, []);
-  return null;
-}
--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className={`py-4 mt-5 ${typeof window !== 'undefined' && document.body.getAttribute('data-theme') === 'dark' ? 'bg-dark text-light' : 'bg-light'}`}>
-      <div className="container text-center">
-        <small>&copy; {new Date().getFullYear()} Mohammed Aymane Saber. All rights reserved.</small>
-      </div>
-    </footer>
-  );
-}
